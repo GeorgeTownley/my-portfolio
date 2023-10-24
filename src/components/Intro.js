44a2cde5814566b7478546a247d27fe11445e883
George Townley <george@knowlesavenue.com>
2023-10-24 15:12:52 +0100

    added intro and projects

---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div id="intro">
+      <h1>Hi, I'm George, a junior software developer</h1>
+      <button>Hire me!</button>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div id="projects">
+      <div className="project-box">
+        <img src="project1.jpg" alt="Project 1" />
+        <h2>Project 1</h2>
+        <p>Small description</p>
+      </div>
+      {/* Add more project boxes here */}
+    </div>
+  );
+};
+
+export default Projects;
