8c5c99e937c417b19cb4f6efa0c61dc35b409a31
George Townley <george@knowlesavenue.com>
2023-10-27 13:46:33 +0100

    improving projects ui

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,9 +1,32 @@
 const projects = [
   {
     id: 1,
-    image: "project1.jpg",
+    image: "./test.png",
     title: "Project 1",
-    description: "Small description",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non consectetur orci, sed finibus sapien. Mauris et ligula in eros consequat lacinia. Nullam at pellentesque massa. Aenean sit amet arcu mattis, egestas est in, dapibus odio. Integer euismod, turpis vitae sagittis tristique, odio felis egestas purus, a fringilla mauris diam a elit. Integer quis sapien quis justo condimentum pulvinar. Cras in arcu id magna dictum ultrices. In sodales dui ante, vel posuere justo elementum eu. Mauris accumsan fermentum suscipit. Phasellus posuere mattis magna, nec fringilla sapien pellentesque in. Proin ultrices nulla rhoncus lectus dapibus malesuada.",
+    tags: ["React", "Express", "Spotify API", "Heroku"],
+    demoLink: "https://demo-link.com",
+    repoLink: "https://github.com/your-username/project1",
+  },
+
+  {
+    id: 2,
+    image: "./test.png",
+    title: "Project 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non consectetur orci, sed finibus sapien. Mauris et ligula in eros consequat lacinia. Nullam at pellentesque massa. Aenean sit amet arcu mattis, egestas est in, dapibus odio. Integer euismod, turpis vitae sagittis tristique, odio felis egestas purus, a fringilla mauris diam a elit. Integer quis sapien quis justo condimentum pulvinar. Cras in arcu id magna dictum ultrices. In sodales dui ante, vel posuere justo elementum eu. Mauris accumsan fermentum suscipit. Phasellus posuere mattis magna, nec fringilla sapien pellentesque in. Proin ultrices nulla rhoncus lectus dapibus malesuada.",
+    tags: ["React", "Express", "Spotify API", "Heroku"],
+    demoLink: "https://demo-link.com",
+    repoLink: "https://github.com/your-username/project1",
+  },
+
+  {
+    id: 3,
+    image: "./test.png",
+    title: "Project 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non consectetur orci, sed finibus sapien. Mauris et ligula in eros consequat lacinia. Nullam at pellentesque massa. Aenean sit amet arcu mattis, egestas est in, dapibus odio. Integer euismod, turpis vitae sagittis tristique, odio felis egestas purus, a fringilla mauris diam a elit. Integer quis sapien quis justo condimentum pulvinar. Cras in arcu id magna dictum ultrices. In sodales dui ante, vel posuere justo elementum eu. Mauris accumsan fermentum suscipit. Phasellus posuere mattis magna, nec fringilla sapien pellentesque in. Proin ultrices nulla rhoncus lectus dapibus malesuada.",
     tags: ["React", "Express", "Spotify API", "Heroku"],
     demoLink: "https://demo-link.com",
     repoLink: "https://github.com/your-username/project1",
