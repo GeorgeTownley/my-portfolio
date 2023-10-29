a7d1a351019e7ff73bbf71e48f4f0959a673beac
George Townley <george@knowlesavenue.com>
2023-10-29 12:22:13 +0000

    change styling of projects + add fanfare info

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,13 +1,13 @@
 const projects = [
   {
     id: 1,
-    image: "./test.png",
-    title: "Project 1",
+    image: "./fanfare.png",
+    title: "Fanfare - Discord Bot",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non consectetur orci, sed finibus sapien. Mauris et ligula in eros consequat lacinia. Nullam at pellentesque massa. Aenean sit amet arcu mattis, egestas est in, dapibus odio. Integer euismod, turpis vitae sagittis tristique, odio felis egestas purus, a fringilla mauris diam a elit. Integer quis sapien quis justo condimentum pulvinar. Cras in arcu id magna dictum ultrices. In sodales dui ante, vel posuere justo elementum eu. Mauris accumsan fermentum suscipit. Phasellus posuere mattis magna, nec fringilla sapien pellentesque in. Proin ultrices nulla rhoncus lectus dapibus malesuada.",
-    tags: ["React", "Express", "Spotify API", "Heroku"],
-    demoLink: "https://demo-link.com",
-    repoLink: "https://github.com/your-username/project1",
+      "A Discord bot made to solve the problem of not knowing who had joined discord calls. The Bot assigns mp3 files to usernames and plays the relevant mp3 file when a user joins a voice channel.",
+    tags: ["SDK", "Node"],
+    demoLink: "",
+    repoLink: "https://github.com/GeorgeTownley/discord-bot",
   },
 
   {
